1aa6f267c6f657fbda7f3f4c1bd1ad2c6696d599
Roz <roz_E4@hotmail.com>
2020-06-21 20:22:31 +0800

    changes to server.js for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -26,3 +26,7 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+mongoose.connect(MONGODB_URI)
